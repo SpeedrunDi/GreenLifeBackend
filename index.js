221ed6d1eb89b3466e40f9edb366e6d2ec79a105
Aman <amanmaksat0@gmail.com>
2023-02-17 12:29:22 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const exitHook = require('async-exit-hook')
+const cookieParser = require('cookie-parser')
+
+const config = require('./config')
+const users = require('./app/users')
+
+const app = express()
+const port = 8000
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: ['http://localhost:3000'],
+}))
+
+app.use('/users', users)
+
+const run = async () => {
+  await mongoose.connect(config.mongo.db, config.mongo.options)
+  console.log('Mongo connected')
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`)
+  })
+
+  exitHook(() => {
+    mongoose.disconnect()
+    console.log('MongoDb disconnect')
+  })
+}
+
+run().catch(e => console.error(e))
